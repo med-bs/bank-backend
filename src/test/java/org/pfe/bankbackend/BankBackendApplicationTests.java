0a5ee5754034eb29e23d4ed25482e93e622ed724
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-02-16 13:48:29 +0100

    init project

---
+++ src/test/java/org/pfe/bankbackend/BankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pfe.bankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
