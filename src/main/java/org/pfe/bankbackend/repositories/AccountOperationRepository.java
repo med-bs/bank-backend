4510adfc2a7dd62f01025ce6d9ce6afda8f0418f
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-02-20 15:39:53 +0100

    add find list operation by accout id + pagination

--- src/main/java/org/pfe/bankbackend/repositories/AccountOperationRepository.java
+++ src/main/java/org/pfe/bankbackend/repositories/AccountOperationRepository.java
@@ -1,8 +1,13 @@
 package org.pfe.bankbackend.repositories;
 
 import org.pfe.bankbackend.entities.AccountOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
+import java.util.List;
 
+public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
+    List<AccountOperation> findByBankAccountId(String accountId);
+    Page<AccountOperation> findByBankAccountIdOrderByOperationDateDesc(String accountId, Pageable pageable);
 }
