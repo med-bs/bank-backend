74935dcd290ca87dd5a823513293d7bccf7f9a07
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-02-20 17:32:41 +0100

    add find search client by name (like)

--- src/main/java/org/pfe/bankbackend/repositories/ClientRepository.java
+++ src/main/java/org/pfe/bankbackend/repositories/ClientRepository.java
@@ -2,7 +2,12 @@ package org.pfe.bankbackend.repositories;
 
 import org.pfe.bankbackend.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
-
+    @Query("select c from Client c where c.name like :kw")
+    List<Client> searchClient(@Param("kw") String keyword);
 }
