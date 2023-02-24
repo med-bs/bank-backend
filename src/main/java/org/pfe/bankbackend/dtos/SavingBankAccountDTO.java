2e0ae154a78071ae11c0bc0ae5f94c11d06f1ca3
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-02-24 17:39:44 +0100

    add dtos(client, bankAcc, savingAcc, currentAcc, credit, debit)

---
+++ src/main/java/org/pfe/bankbackend/dtos/SavingBankAccountDTO.java
@@ -0,0 +1,16 @@
+package org.pfe.bankbackend.dtos;
+
+import lombok.Data;
+import org.pfe.bankbackend.enums.AccountStatus;
+
+import java.util.Date;
+
+@Data
+public class SavingBankAccountDTO extends BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private ClientDTO clientDTO;
+    private double interestRate;
+}
