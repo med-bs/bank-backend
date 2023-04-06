2e94f6847fce8c38e5bf5068c1bd4d66b40123ca
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-04-06 02:01:27 +0100

    add find by client id in repository

--- src/main/java/org/pfe/bankbackend/repositories/BankAccountRepository.java
+++ src/main/java/org/pfe/bankbackend/repositories/BankAccountRepository.java
@@ -3,5 +3,11 @@ package org.pfe.bankbackend.repositories;
 import org.pfe.bankbackend.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+import java.util.List;
+import java.util.Optional;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+    List<BankAccount> findByClient_Id(Long clientId);
+
+    Optional<BankAccount> findByIdAndClient_Id(String id, Long clientId);
 }
